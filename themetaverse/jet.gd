cbb088f295c347309f2a60d836a1af4f72993985
Bryan Duggan <skooter500@gmail.com>
2023-11-01 23:26:57 +0000

    wip

--- themetaverse/jet.gd
+++ themetaverse/jet.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @onready var player:RigidBody3D = $"../../.."
 
+@onready var flames:GPUParticles3D = $GPUParticles3D
+
 @export var power = 100 
 
 @export var trigger = 0 
@@ -9,8 +11,11 @@ extends Node3D
 func _physics_process(delta):
 	if trigger > 0:
 		var force = - global_transform.basis.y * power * trigger
-		DebugDraw.draw_arrow_line(global_transform.origin, global_transform.origin + force, Color.ORANGE_RED, 0.1)
 		player.apply_force(-force, Vector3.ZERO)
+		flames.emitting = true
+		flames.amount = max(500 * trigger, 1)
+	else:
+		flames.emitting = false
 
 func _on_xr_controller_3d_input_float_changed(name, value):
 	if name == "trigger":
