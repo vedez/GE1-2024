6e9d37cee292e0af123adb85ee945cb5d5dbad10
Bryan Duggan <skooter500@gmail.com>
2023-11-22 20:25:31 +0000

    gravity gun wip

---
+++ themetaverse/gravity_gun.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var holdDistance = 1
+public float maximumVelocity = 30;
+public float powerFactor = 40;
+
+GameObject pickedUp = null;
+Transform camera;
+
+public bool isPhisGun = false;
+
+
