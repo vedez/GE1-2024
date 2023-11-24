e443f96e712e67d03cf6f6b396c10390acf34e16
Bryan Duggan <skooter500@gmail.com>
2023-11-24 12:53:22 +0000

    phy

--- themetaverse/TPController.gd
+++ themetaverse/TPController.gd
@@ -15,6 +15,6 @@ func _ready():
 func _process(delta):
 	
 	# transform.origin += transform.basis.z
-	#player.global_position = lerp(player.global_position, target.global_position, delta * 5)
-	# player.look_at(target.get_parent_node_3d().global_position, Vector3.UP)
+	player.global_position = lerp(player.global_position, target.global_position, delta * 5)
+	player.look_at(target.get_parent_node_3d().global_position, Vector3.UP)
 	pass
